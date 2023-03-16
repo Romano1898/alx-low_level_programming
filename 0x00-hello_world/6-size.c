04299ad80f586ec2a4d105fd622e985c83670604
romano.naidoo@gmail.com <romano.naidoo@gmail.com>
2023-03-16 16:17:51 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,9 @@ int intType;
 float floatType;
 double doubleType;
 char charType;
-
 printf("Size of a char: %lu \n", sizeof(intType));
 printf("Size of float %lu \n", sizeof(floatType));
 printf("Size of double %lu \n", sizeof(doubleType));
 printf("Size of char %lu \n", sizeof(charType));
- return (0);
+return (0);
 }
