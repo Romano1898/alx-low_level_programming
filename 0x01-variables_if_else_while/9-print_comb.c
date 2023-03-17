b77a4432530d85e67a9ed3730ef35108520a7cb0
romano.naidoo@gmail.com <romano.naidoo@gmail.com>
2023-03-17 14:29:08 -0700

    Displays all combinations of single digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,12 @@
 int main(void)
 {
 int al;
-for (al = 0; al <= 9; al++)
+for (al = 0; al <= 8; al++)
 {
 putchar('0' + al);
 putchar(',');
 putchar(' ');
 }
-putchar('\n');
+putchar('9');
 return (0);
 }
