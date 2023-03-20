ad9a75d46820c9d28c98accc3bb930938c49144d
romano.naidoo@gmail.com <romano.naidoo@gmail.com>
2023-03-20 15:05:26 -0700

    Prints absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _abs - Prints absolute value of a number
+*
+* Return: Always 0 (Success)
+*@n: Number to be checked
+*/
+
+int _abs(int n)
+{
+if (n >= 0)
+{
+_putchar(n);
+}
+else
+{
+int f = -1 * n;
+_putchar(f);
+}
+return (0);
+}
