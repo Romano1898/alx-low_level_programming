d2e5ecf00a3af01e98aabceb38845e0213a50654
romano.naidoo@gmail.com <romano.naidoo@gmail.com>
2023-03-20 15:40:07 -0700

    Prints last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints last digit of a number
+ *
+ * Return: Last digit
+ *@n: Number to be checked
+ */
+
+int print_last_digit(int n)
+{
+int f = n % 10;
+return (f);
+}
