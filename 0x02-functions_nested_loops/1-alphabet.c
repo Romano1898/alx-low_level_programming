3c74044c371cabdffb0d7eeaa679d223bd7d1158
romano.naidoo@gmail.com <romano.naidoo@gmail.com>
2023-03-20 12:54:06 -0700

    Prints alphabet;

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-print_alphabet(void)
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+print_alphabet(char)
 {
 for (char al = 'a'; al <= 'z'; ++al)
 {
 _putchar(al);
 }
+_putchar('\n');
 return (0);
 }
