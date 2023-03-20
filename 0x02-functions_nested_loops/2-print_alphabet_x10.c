fce356cf24b4dcffe73f1df0c2b2e9d10d620656
romano.naidoo@gmail.com <romano.naidoo@gmail.com>
2023-03-20 13:46:57 -0700

    Prints alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - Prints alphabet ten times
+*/
+
+void print_alphabet_x10(void)
+for (int i = 0; i < 10; i++)
+{
+for (char al = 'a'; al <= 'z'; ++al)
+{
+_putchar(al);
+}
+}
