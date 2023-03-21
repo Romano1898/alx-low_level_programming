669bc21fb22e8c6d037ac507e33a5c88c9666df0
romano.naidoo@gmail.com <romano.naidoo@gmail.com>
2023-03-21 06:05:59 -0700

    Prints 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - Prints 9 times table from 0
+ */
+
+void times_table(void)
+{
+int a, b, c;
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+c = a * b;
+if (c <= 9)
+{
+_putchar('0' + (c));
+_putchar(',');
+_putchar(' ');
+}
+if (c > 9)
+{
+_putchar(c / 10);
+_putchar(c % 10);
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
