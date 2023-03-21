bb42f7cc22d4794382a02ea7ea864eb801c57a1a
romano.naidoo@gmail.com <romano.naidoo@gmail.com>
2023-03-20 18:47:17 -0700

    Prints 24 hour time cycle

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,22 +7,22 @@
 
 void jack_bauer(void)
 {
-int a, b, c, d;
-for (a = 0; a <= 2; a++)
-{
-for (b = 0; b <= 3; b++)
+int b, c, d;
+for (b = 0; b <= 23; b++)
 {
 for (c = 0; c <= 5; c++)
 {
 for (d = 0; d <= 9; d++)
 {
-_putchar('0' + a);
+if (b < 10)
+{
+_putchar('0');
+}
 _putchar('0' + b);
 _putchar(':');
 _putchar('0' + c);
 _putchar('0' + d);
 _putchar('\n');
-}
 }
 }
 }
