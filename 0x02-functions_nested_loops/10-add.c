387e1d02cfbf36365a96fcfda5b506bb99b2a81a
romano.naidoo@gmail.com <romano.naidoo@gmail.com>
2023-03-21 18:00:03 -0700

    Adds two integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,9 +4,11 @@
 * add - Adds two integers
 *
 * Return: sum of numbers
+*@a: first integer
+*@b: second integer
 */
 
-int add (int a, int b)
+int add(int a, int b)
 {
 int sum = a + b;
 return (sum);
