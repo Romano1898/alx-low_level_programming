38ed869564b84ecdb939d7cfa7ce7676ed13cc12
Romano1898 <romano.naidoo@gmail.com>
2023-03-23 21:53:38 +0200

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include
+
+/**
+ *print_numbers - Prints prime numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+_putchar(i); 
+}
+}
