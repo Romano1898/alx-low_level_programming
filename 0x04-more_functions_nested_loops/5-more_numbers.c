a14dc09f5bcfcae6415623140f01691a241739ad
Romano1898 <romano.naidoo@gmail.com>
2023-03-24 00:16:16 +0200

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,7 +9,7 @@ void more_numbers(void)
 {
 int num;
 int count;
-for (count = 0; count <= 14; ++count)
+for (count = 0; count <= 10; ++count)
 {
 for (num = 0; num <= 14; ++num)
 {
@@ -18,7 +18,7 @@ for (num = 0; num <= 14; ++num)
 _putchar((num / 10) + '0');
 _putchar((num % 10) + '0');
 }
- else
+else
 {
 _putchar('0' + num);
 }
