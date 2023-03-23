0dc2e709dd6d38393d1ca598aad44dd2fcb6293b
Romano1898 <romano.naidoo@gmail.com>
2023-03-24 01:16:18 +0200

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - Prints lines based on n
+ *
+ *@n: number of underscores
+ */
+
+void print_line(int n)
+{
+int c;
+for (c = 0; c < n; c++)
+{
+_putchar('95');
+}
+}
