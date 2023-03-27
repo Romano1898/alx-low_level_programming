06037463476498bf25086423bfbcdab185408675
Romano1898 <romano.naidoo@gmail.com>
2023-03-28 01:40:37 +0200

    Print string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,15 +8,15 @@
 
 void print_rev(char *s)
 {
-int i;
-i = 0;
+	int i = 0;
 
-while (s[i] != '\0')
+	while (i >= 0)
 {
-i++;
+	if (s[i] == '\0')
+	break;
+	i++;
 }
-for (i--; s[i] >= 0; i--)
-{
-_putchar(s[i]);
-}
+	for (i--; i >= 0; i--)
+	_putchar(s[i]);
+	_putchar('\n');
 }
