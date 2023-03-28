5f473c9f201b1ce2bd480c8935d9da592d95796f
Romano1898 <romano.naidoo@gmail.com>
2023-03-28 03:30:21 +0200

    Outputs int array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_array - Prints array of integers
+ *
+ * @a: holds value of array at position n
+ * @n: number of elements of array
+ */
+
+void print_array(int *a, int n)
+{
+  int i;
+
+  for (i = 0; i < (n - 2); i++)
+    {
+      printf ("%d", a[i]);
+    }
+  printf ("%d\n", a[n - 1]);
+}
