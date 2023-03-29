8ba6d62968c98d8b9e49e660c21c4c899caf83ff
Romano1898 <romano.naidoo@gmail.com>
2023-03-29 21:02:29 +0200

    Concatenate string with byte limit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,25 +3,27 @@
 /**
  * *_strncat - Concatenates two strings
  *
- * @dest - Destination string
- * @src - Source string to be concatenated with dest
+ * @dest: Destination string
+ * @src: Source string to be concatenated with dest
+ * @n: Byte limit
  *
  * Return: Concatenated string
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-int i = 0, j = 0;
+  int i = 0, j = 0, k = 0;
 
 while (dest[i] != '\0' && i <= n)
 {
 i++;
 }
-while (src[j] != '\0')
+
+while (src[j] != '\0' & k <= i)
 {
 dest[i] = src[j];
 j++;
-i++;
+k++;
 }
 return (dest);
 }
