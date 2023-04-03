04c2505a6ff99118923dde2b2d03b807bdb1a9b4
Romano1898 <romano.naidoo@gmail.com>
2023-04-03 23:53:26 +0200

    Strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,14 +9,14 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-  char *p, *c, *s;
+char *p, *c, *s;
 
 for (p = haystack; *p != '\0'; p++)
 {
 for (c = p, s = needle; *s != '\0' && *c == *s; c++, s++)
 {
 }
-if (*r == '\0')
+if (*s == '\0')
 {
 return (p);
 }
