c1f137da0b8e63e1cd24b185f07859eef13bcd36
Romano1898 <romano.naidoo@gmail.com>
2023-04-03 21:06:19 +0200

    Strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * unsigned int _strspn - Gets length of a prefix substring
+ * @s: String whose prefix will be measured
+ * @accept: Substring prefix to look for in s
+ * Return: number of bytes in prefix of s
+ */
+
+unsigned int _strspn(char *s, char *accept
+{
+  unsigned int i, j;
+
+if (s == '\0' && accept == '\0')
+{
+return (0);
+}
+else
+{
+for (i = 0;s[i] !=m'\0'; i++)
+{
+for (j = 0;accept[j] != '\0'; j++)
+{
+if (s[i] == accept [j]
+{
+break;
+}
+}
+if (accept[j] == '\0')
+{
+return (i);
+}
+}
+return (i);
+}
+}
