09e6bf5f3d76003229d926c6fa0018b5943a8740
Romano1898 <romano.naidoo@gmail.com>
2023-04-06 03:54:56 +0200

    Prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * primedet - Checks if a number is prime
+ * @n: Input integer
+ * @i: Possible prime factors of n
+ * Return: 1 if prime. 0 if not.
+ */
+
+int primedet(int n, int i)
+{
+if (n <= 2)
+{
+return (n == 2);
+}
+else if (n % i == 0)
+{
+return (0);
+}
+else if (i * i > n)
+{
+return (1);
+}
+else
+{
+return (primedet(n, i + 1));
+}
+}
+
+/**
+* is_prime - Checks if number is 1 or not
+* @n: Input integer
+* Return: Pushes to primedet
+*/
+
+int is_prime(int n)
+{
+if (n <= 1)
+{
+return (0);
+}
+return (primedet(n, 2));
+}
