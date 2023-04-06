3b7903a5110524555ef15924e5739c2bf545627d
Romano1898 <romano.naidoo@gmail.com>
2023-04-06 02:02:55 +0200

    Power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,16 +9,16 @@
 
 int _pow_recursion(int x, int y)
 {
-if (y < 0)
-{
-return (-1);
-}
-else if (y == 0)
+if (y == 0)
 {
 return (1);
 }
+else if (y > 0)
+{
+return (x * _pow_recursion(x, y - 1));
+}
 else
 {
-return (_pow_recursion(x, (y - 1) * x));
+return ((1 / x) * _pow_recursion(x, y + 1));
 }
 }
