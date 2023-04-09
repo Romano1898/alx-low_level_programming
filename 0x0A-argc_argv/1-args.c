d626c81520cb0619e300d3314cad59d6152e9c4e
Romano1898 <romano.naidoo@gmail.com>
2023-04-10 01:50:09 +0200

    Count args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc - Argument count
+ * @argv - Argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d", argc);
+return (0);
+}
