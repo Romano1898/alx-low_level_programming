1f86912a91544f294c0945cd994394c369aea045
Romano1898 <romano.naidoo@gmail.com>
2023-04-10 02:31:04 +0200

    Multiply

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,16 +12,19 @@
 
 int main(int argc, char *argv[])
 {
-if (argc != 3)
-{
-return (1);
-}
 int num1;
 int num2;
- int prod;
+int prod;
+
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
 prod = num1 *num2;
 printf("%d\n", prod);
 return (0);
+
 }
