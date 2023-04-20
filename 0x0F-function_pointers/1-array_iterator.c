74208810a22192caa2b7b6c88fd2232862e56f2f
Romano1898 <romano.naidoo@gmail.com>
2023-04-20 03:09:59 +0200

    Array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,7 +3,7 @@
 
 /**
  * array iterator - Displays each element of an array given as a pointer
- * 
+ *
  * @array: pointer to array
  * @size: size of array
  * @action: function that displays an iteration of an array
@@ -12,7 +12,7 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t i;
-if (array != NULL || action != NULL)
+if (size != 0 || array != NULL || action != NULL)
 {
 for (i = 0; i < size; i++)
 {
