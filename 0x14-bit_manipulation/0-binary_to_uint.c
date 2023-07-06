361116c2f3e363942ab170e7126c088e0320d896
Romano1898 <romano.naidoo@gmail.com>
2023-07-06 23:36:04 +0200

    1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,24 +11,24 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int num = 0;
-int i = 0;
+	int i = 0;
+	unsigned int num = 0;
 
-if (b == NULL)
-return (0);
+	if (b == NULL)
+	return (0);
 
-while (b != '\0')
-{
-if (b[i] == '0' || b[i] == '1')
-{
-num <<= 1;
-num += b[i] - '0';
-i++;
-}
-else
-{
-return (0);
-}
-}
-return (num);
+	while (b[i] != '\0')
+	{
+	if (b[i] == '0' || b[i] == '1')
+		{
+		num <<= 1;
+		num += b[i] - '0';
+		i++;
+		}
+		else
+		{
+		return (0);
+		}
+	}
+	return (num);
 }
