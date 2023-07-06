5fff59975a7707a2e88131517e9897c0024cafcc
Romano1898 <romano.naidoo@gmail.com>
2023-07-06 23:15:53 +0200

    1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,13 +15,13 @@ unsigned int num = 0;
 int i = 0;
 
 if (b == NULL)
-return 0;
+return (0);
 
 while (b[i] == '0' || b[i] == '1')
 {
 num <<= 1;
-num += b[i]-'0';
+num += b[i] - '0';
 i++;
 }
-return num;
+return (num);
 }
