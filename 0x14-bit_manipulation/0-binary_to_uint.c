2c74b96cd4726f74383c7b6ef432d95a993ccc8b
Romano1898 <romano.naidoo@gmail.com>
2023-07-06 23:28:32 +0200

    1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,15 +17,14 @@ int i = 0;
 if (b == NULL)
 return (0);
 
+while (b != '\0')
+{
 if (b[i] == '0' || b[i] == '1')
 {
 num <<= 1;
 num += b[i] - '0';
 i++;
 }
-else
-{
-return (0);
 }
 return (num);
 }
